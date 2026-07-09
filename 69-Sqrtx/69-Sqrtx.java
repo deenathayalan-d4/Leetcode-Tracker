55f5819964579c1026a1de0cdaf8a1db0db1c6be
deenathayalan-d4 <d.deenathayalan5407@gmail.com>
2026-07-09 15:09:14 +0530

    Create 69-Sqrtx

---
+++ 69-Sqrtx/69-Sqrtx.java
@@ -0,0 +1,9 @@
+// Last updated: 09/07/2026, 15:09:14
+class Solution {
+    public int mySqrt(int x) {
+        if(x>=0)
+        return (int)Math.sqrt(x);
+        else
+        return 0;
+    }
+}
