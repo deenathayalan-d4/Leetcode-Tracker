e259b192c3fd501ab41168f37c784df63cd3f4c5
deenathayalan-d4 <d.deenathayalan5407@gmail.com>
2026-07-09 15:08:14 +0530

    Create 197-RisingTemperature

---
+++ 197-RisingTemperature/197-RisingTemperature.sql
@@ -0,0 +1,7 @@
+-- Last updated: 09/07/2026, 15:08:13
+# Write your MySQL query statement below
+SELECT w1.id 
+FROM Weather w1
+JOIN Weather w2 
+ON DATEDIFF(w1.recordDate, w2.recordDate) = 1
+WHERE w1.temperature > w2.temperature;
