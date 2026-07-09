a7af0391e6226599a4b5cb950b770e7fd5a4e0f9
deenathayalan-d4 <d.deenathayalan5407@gmail.com>
2026-07-09 15:06:12 +0530

    Create 2383-AddTwoIntegers

---
+++ 2383-AddTwoIntegers/2383-AddTwoIntegers.java
@@ -0,0 +1,8 @@
+// Last updated: 09/07/2026, 15:06:11
+class Solution {
+    public int sum(int num1, int num2) {
+        int ans=num1+num2;
+       System.out.println(ans);
+       return ans;
+    }
+}
