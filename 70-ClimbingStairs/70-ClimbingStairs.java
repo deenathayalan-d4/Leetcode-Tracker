701e3f8fc44a7fecec675e6d18094c2b160fce40
deenathayalan-d4 <d.deenathayalan5407@gmail.com>
2026-07-09 15:09:13 +0530

    Create 70-ClimbingStairs

---
+++ 70-ClimbingStairs/70-ClimbingStairs.java
@@ -0,0 +1,19 @@
+// Last updated: 09/07/2026, 15:09:12
+class Solution {
+    public int climbStairs(int n) {
+        if (n <= 2) {
+            return n;
+        }
+
+        int a = 1;
+        int b = 2;
+
+        for (int i = 3; i <= n; i++) {
+            int c = a + b;
+            a = b;
+            b = c;
+        }
+
+        return b;
+    }
+}
