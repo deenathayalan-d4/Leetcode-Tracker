b1dfc4aae1c04693ef5413955ecac463e57167c7
deenathayalan-d4 <d.deenathayalan5407@gmail.com>
2026-07-09 15:06:14 +0530

    Create 2121-FindIfPathExistsInGraph

---
+++ 2121-FindIfPathExistsInGraph/2121-FindIfPathExistsInGraph.java
@@ -0,0 +1,26 @@
+// Last updated: 09/07/2026, 15:06:13
+class Solution {
+    public boolean validPath(int n, int[][] e, int s, int d) {
+        List<List<Integer>> adj=new ArrayList<>();
+        for(int i=0;i<n;i++) adj.add(new ArrayList<>());
+        int m=e.length;
+        for(int i=0;i<m;i++){
+            int sv=e[i][0];
+            int ev=e[i][1];
+            adj.get(sv).add(ev);
+            adj.get(ev).add(sv);
+        }
+        boolean[] vis=new boolean[n];
+        dfs(adj, vis,s);
+        if(vis[s]==true && vis[d]==true) return true;
+        return false;
+    }
+    void dfs(List<List<Integer>> adj, boolean[] vis, int s){
+        vis[s]=true;
+        int m=adj.get(s).size();
+        for(int i=0;i<m;i++){
+            int x=adj.get(s).get(i);
+            if(vis[x]==false) dfs(adj, vis, x);
+        }
+    }
+}
