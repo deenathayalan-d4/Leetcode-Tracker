c198e7120c2cd83031d1c78465800b9940e494fe
deenathayalan-d4 <d.deenathayalan5407@gmail.com>
2026-07-09 15:09:31 +0530

    Create 45-JumpGameIi

---
+++ 45-JumpGameIi/45-JumpGameIi.java
@@ -0,0 +1,21 @@
+// Last updated: 09/07/2026, 15:09:30
+class Solution {
+    public int jump(int[] nums) {
+
+        int jumps = 0;
+        int end = 0;
+        int farthest = 0;
+
+        for (int i = 0; i < nums.length - 1; i++) {
+
+            farthest = Math.max(farthest, i + nums[i]);
+
+            if (i == end) {
+                jumps++;
+                end = farthest;
+            }
+        }
+
+        return jumps;
+    }
+}
