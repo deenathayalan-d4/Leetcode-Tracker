3c0daf770d3e33973285ed0d9ef43e52a05e0377
deenathayalan-d4 <d.deenathayalan5407@gmail.com>
2026-07-09 15:08:36 +0530

    Create 176-SecondHighestSalary

---
+++ 176-SecondHighestSalary/176-SecondHighestSalary.sql
@@ -0,0 +1,9 @@
+-- Last updated: 09/07/2026, 15:08:35
+# Write your MySQL query statement below
+SELECT
+(
+    SELECT DISTINCT salary
+    FROM Employee
+    ORDER BY salary DESC
+    LIMIT 1 OFFSET 1
+) AS SecondHighestSalary;
